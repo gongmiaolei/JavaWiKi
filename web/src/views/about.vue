c692b143d6940138383fd32f423f9a1490f25597
CoderLei <CoderLei@qq.com>
2023-03-21 10:56:41 +0800

    WiKi项目

---
+++ web/src/views/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="about">
+        <h2>我是巩淼磊</h2>
+        <br>
+        <div><b>微信号：13673965926</b></div>
+        <div>
+          <img src="/image/jiawaquanzhan.jpg">
+        </div>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<style>
+  .about {
+    line-height: 30px;
+    padding: 40px;
+    background-color: #f5f5f5;
+    font-size: 16px;
+  }
+  .about h2 {
+    margin-bottom: 30px;
+    font-weight: 700;
+  }
+  .about img {
+    margin-top: 10px;
+  }
+</style>
